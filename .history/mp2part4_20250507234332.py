912cc8cb87aa6bb18de949efe1d2f18e8c1545ca
WIN-N7B62BIC95E\Administrator <202312632@fit.edu.ph>
2025-05-07 23:47:34 +0800

    History changes

---
+++ .history/mp2part4_20250507234332.py
@@ -0,0 +1,39 @@
+from random import randint
+from mp2function import *
+
+listMenu = [
+    "Play Number Guessing Game"
+    ]
+
+def menu():
+    while True:
+        displayMenu(listMenu)
+        try:
+            match(menuPrompt(listMenu)):
+                case 1:
+                    play()
+                case False:
+                    return
+                case "Error":
+                    continue
+        except:
+            padding("Something happened so try again")
+            continue
+
+def play():
+    answer = randint(1,100)
+    try:
+        choice = None
+        attempt = 0
+        while answer != choice:
+            choice = dataTypeInput("Guess the number: ", "int")
+            attempt += 1
+            if choice > answer:
+                print("Too high!")
+            elif choice < answer:
+                print("Too low!")
+            else:
+                print(f"Congratulations! You guessed the number in {attempt} attempts.")
+    except:
+        print("Wrong data type input, try again!")
+
